a2d0cc7f9be754a609ae19ef480a004c8e923aa6
Khyle Vincent Cardosa <kvcards26@gmail.com>
2022-10-14 22:49:13 +0800

    Add project files.

---
+++ Automatic Fan Controller/Controller.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatic_Fan_Controller
+{
+    public  class Controller : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool _isAutoMode = true;
+        private bool _isManualMode = false;
+        private bool _isSearchingPort = true;
+        private bool _isPortFound = true;
+        private bool _isConnected = false;
+        private int _peopleCount = 0;
+        private int _temperature = 0;
+        private int _activationTemp = 25;
+        private int _fanSpeed = 0;
+        private int _startFanSpeed = 50;
+
+        public bool IsAutoMode
+        {
+            get { return _isAutoMode; }
+            set 
+            { 
+                _isAutoMode = value;
+                OnPropertyChanged("IsAutoMode");
+            }
+        }
+
+        public bool IsManualMode
+        {
+            get { return _isManualMode; }
+            set
+            {
+                _isManualMode = value;
+                OnPropertyChanged("IsManualMode");
+            }
+        }
+
+        public bool IsSearchingPort
+        { 
+            get { return _isSearchingPort; }
+            set
+            {
+                _isSearchingPort = value;
+                OnPropertyChanged("IsSearchingPort");
+            }
+        }
+
+        public bool IsPortFound
+        {
+            get { return _isPortFound; }
+            set
+            {
+                _isPortFound = value;
+                OnPropertyChanged("IsPortFound");
+            }
+        }
+
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            set
+            {
+                _isConnected = value;
+                OnPropertyChanged("IsConnected");
+            }
+        }
+
+        public int PeopleCount
+        {
+            get { return _peopleCount; }
+            set 
+            { 
+                _peopleCount = value;
+                OnPropertyChanged("PeopleCount");
+            }
+        }
+
+        public int Temperature
+        {
+            get { return _temperature; }
+            set 
+            { 
+                _temperature = value;
+                OnPropertyChanged("Temperature");
+            }
+        }
+
+        public int ActivationTemp
+        {
+            get { return _activationTemp; }
+            set 
+            { 
+                _activationTemp = value;
+                OnPropertyChanged("ActivationTemp");
+            }
+        }
+
+        public int FanSpeed
+        {
+            get { return _fanSpeed; }
+            set 
+            { 
+                _fanSpeed = value;
+                OnPropertyChanged("FanSpeed");
+            }
+        }
+
+        public int StartFanSpeed
+        { 
+            get { return _startFanSpeed; }
+            set 
+            {
+                if (value > 0 && value < 100)
+                {
+                    _startFanSpeed = value;
+                    OnPropertyChanged("StartFanSpeed");
+                }
+            }
+        }
+
+        public string? GetArduinoPort()
+        {
+            ManagementScope connectionScope = new();
+            SelectQuery serialQuery = new("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new(connectionScope, serialQuery);
+            try
+            {
+                foreach (ManagementObject item in searcher.Get().Cast<ManagementObject>())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException)
+            {
+                /* Do Nothing */
+            }
+
+            return null;
+        }
+
+        public void ParseDataFromSerial(string serialData)
+        {
+
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Automatic Fan Controller/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Automatic_Fan_Controller
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Controller _controller = new();
+        private readonly SerialPort _serialPort = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = _controller;
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(_serialPort_DataReceived);
+            ConnectArduinoPortAsync();
+        }
+
+        private async void ConnectArduinoPortAsync()
+        {
+            _controller.IsSearchingPort = true;
+            await Task.Delay(3000);
+
+            string? arduinoPort = _controller.GetArduinoPort();
+
+            if (arduinoPort is not null)
+            {
+                _serialPort.PortName = arduinoPort;
+                _serialPort.BaudRate = 9600;
+                _serialPort.Open();
+
+                _controller.IsPortFound = true;
+                _controller.IsConnected = true;
+            }
+
+            _controller.IsSearchingPort = false;
+        }
+
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string controllerData = _serialPort.ReadLine();
+            _controller.ParseDataFromSerial(controllerData);
+        }
+
+       
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void radioAutoMode_Checked(object sender, RoutedEventArgs e)
+        {
+            _controller.IsManualMode = false;
+            _controller.IsAutoMode = true;
+        }
+
+        private void radioManualMode_Checked(object sender, RoutedEventArgs e)
+        {
+            _controller.IsManualMode = true;
+            _controller.IsAutoMode = false;
+        }
+
+        private void btnMinusTemperature_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.ActivationTemp--;
+        }
+
+        private void btnAddTemperature_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.ActivationTemp++;
+        }
+
+        private void btnMinusStartFanSpeed_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.StartFanSpeed--;
+        }
+
+        private void btnAddStartFanSpeed_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.StartFanSpeed++;
+        }
+
+        private void btnFanSpeed_Off_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.FanSpeed = 0;
+        }
+
+        private void btnFanSpeed_Max_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.FanSpeed = 99;
+        }
+
+        private void btnFanSpeed_30_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.FanSpeed = 30;
+        }
+
+        private void btnFanSpeed_50_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.FanSpeed = 50;
+        }
+
+        private void btnFanSpeed_70_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.FanSpeed = 70;
+        }
+
+        private void btnFanSpeed_90_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.FanSpeed = 90;
+        }
+
+        private void lblStartFanSpeed_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^1-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void lblActivationTemperature_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
